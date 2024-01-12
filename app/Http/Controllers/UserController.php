26ec11bf1e5962e9dc1d91a014f40fa37e155fbf
kukohgntg <kukohfr@gmail.com>
2024-01-12 12:34:12 +0700

    add client and admin route

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,8 +6,10 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function profile(Request $request)
+    // public function profile(Request $request)
+    public function profile()
     {
+        // $request->session()->flush();
         dd('halaman profile');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\BookControllers;
 use App\Http\Controllers\DashboardController;
 
 /*
@@ -24,5 +25,6 @@ Route::get('login', [AuthController::class, 'login'])->name('login');
 Route::post('login', [AuthController::class, 'authenticating']);
 Route::get('register', [AuthController::class, 'register']);
 
-Route::get('dashboard', [DashboardController::class, 'index'])->middleware('auth');
-Route::get('profile', [UserController::class, 'profile'])->middleware('auth');
+Route::get('dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'only_admin']);
+Route::get('profile', [UserController::class, 'profile'])->middleware(['auth', 'only_client']);
+Route::get('book', [BookControllers::class, 'index']);
