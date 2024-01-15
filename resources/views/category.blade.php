37d3165861c6d2662383cb3d032d684cbd0ee73f
kukohgntg <kukohfr@gmail.com>
2024-01-15 22:10:22 +0700

    add add function on category

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -1,7 +1,40 @@
 @extends('layouts.mainlayout')
 
-@section('title', 'Dashboard')
+@section('title', 'Category')
 
 @section('content')
-    <h1>halaman category</h1>
+    <h1>Category List</h1>
+    <div class="mt-5 d-flex justify-content-end">
+        <a href="category-add" class="btn btn-primary">Add Data</a>
+    </div>
+
+    <div class="mt-5">
+        @if (session('status'))
+            <div class="alert alert-success">{{ session('status') }}</div>
+        @endif
+    </div>
+
+    <div class="my-5">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Name</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($categories as $item)
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ $item->name }}</td>
+                        <td>
+                            <a href="#">edit</a>
+                            <a href="#">delete</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
